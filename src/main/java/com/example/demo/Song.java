aa9a5af866a71e212184fa1deccb5ce808605eb3
Dave Wolf <dave45678@gmail.com>
2018-03-27 10:01:06 -0400

    add Song class

---
+++ src/main/java/com/example/demo/Song.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+public class Song {
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    private long id;
+    private String name;
+    private String artist;
+}
